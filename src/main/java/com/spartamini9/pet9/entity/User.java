71e4b06357db9a723b91d9bb0e07a267f678d833
goodn911 <90505402+goodn911@users.noreply.github.com>
2021-12-06 22:18:05 +0900

    asd

--- src/main/java/com/spartamini9/pet9/entity/User.java
+++ src/main/java/com/spartamini9/pet9/entity/User.java
@@ -1,14 +1,18 @@
 package com.spartamini9.pet9.entity;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @NoArgsConstructor
 @Getter
 @Entity
+@AllArgsConstructor
+@Setter
 public class User {
 
 
